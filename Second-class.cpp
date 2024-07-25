26398eef0a4d95a0451f3c51246f045d554cbbd7
Gautam-Rathour <gau321tam@gmail.com>
2024-07-25 22:00:50 +0530

    Add Somthing

--- Second-class.cpp
+++ Second-class.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main () {
     cout << "Hyy Gautam Rathour\n";
      cout << "Hyy Gautam Rathour\n";
-     cout << "Hyy Saurabh Kumar\n";
+     cout << "Hyy  Kumar\n";
      
      cout << "Hyy Gautam \n";
      cout << "Hello Raj ";
