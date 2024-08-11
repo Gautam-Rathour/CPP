340f1e8d4dd4fb6a7ec2c2ade28a4c0a3d8c3947
Gautam-Rathour <gau321tam@gmail.com>
2024-08-11 10:38:47 +0530

    Delete

--- First-class.cpp
+++ First-class.cpp
@@ -15,7 +15,7 @@ int main(){
     std::cout<<"Hello India";
     std::cout<<"Hello India";
     std::cout<<"Hello";
-    std::cout<<"Hello World\n";
+    std::cout<<"Hello \n";
     std::cout<<"Hello \n";
     std::cout<<"Hello werewr \n";
     std::cout<<"Hello\n";
