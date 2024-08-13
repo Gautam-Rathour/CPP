4575737e37359f68ba1909635c06596c853ca229
Gautam-Rathour <gau321tam@gmail.com>
2024-08-13 12:53:37 +0530

    Add

--- First-class.cpp
+++ First-class.cpp
@@ -12,7 +12,7 @@ int main(){
     std::cout<<"Hello Duniya\n";
     std::cout<<"Hello ";
     std::cout<<"Hello Duniya\n";
-    std::cout<<"Hello India";
+    std::cout<<"Hello ";
     std::cout<<"Hello ";
     std::cout<<"Hello";
     std::cout<<"Hello \n";
